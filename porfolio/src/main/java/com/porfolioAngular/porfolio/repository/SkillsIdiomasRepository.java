b4c908af7f12fa41f5296f985616d730182f79dc
Barbara Barrueta <75133162+barbarabarrueta@users.noreply.github.com>
2022-05-01 00:14:05 -0300

    borrar carpetas repetidas

--- porfolio/src/main/java/com/porfolioAngular/porfolio/repository/SkillsIdiomasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.porfolioAngular.porfolio.repository;
-
-import com.porfolioAngular.porfolio.model.SkillsIdiomas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SkillsIdiomasRepository extends JpaRepository <SkillsIdiomas, Long> {
-    
-}
